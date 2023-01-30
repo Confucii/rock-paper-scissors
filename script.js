c5b7b23f3e77ad5c96613a622bdef8a91a3b3bcc
Oleksii <lesha.avdeev@gmail.com>
2023-01-30 03:43:34 +0200

    Add function to control game flow

--- script.js
+++ script.js
@@ -30,18 +30,58 @@ The string is updated with respect to the match up. */
 
 function calcRoundResults(playerSelection, computerSelection) {
     playerSelection = playerSelection.slice(0, 1).toUpperCase() + playerSelection.slice(1);
-    let gameConclusion;
+    let roundConclusion;
+    let roundResult;
 
     if (playerSelection === computerSelection) {
-        gameConclusion = `Draw! ${playerSelection} equals ${computerSelection}`;
+        roundConclusion = `Draw! ${playerSelection} equals ${computerSelection}`;
+        roundResult = 0;
     } else if ((playerSelection === "Rock" && computerSelection === "Scissors") || 
     (playerSelection === "Scissors" && computerSelection === "Paper") || 
     (playerSelection === "Paper" && computerSelection === "Rock")) {
-        gameConclusion = `You win! ${playerSelection} beats ${computerSelection}`;
+        roundConclusion = `You win! ${playerSelection} beats ${computerSelection}`;
+        roundResult = 1;
     } else {
-        gameConclusion = `You lose! ${computerSelection} beats ${playerSelection}`;
+        roundConclusion = `You lose! ${computerSelection} beats ${playerSelection}`;
+        roundResult = -1;
     }
     
-    return gameConclusion;
+    console.log(roundConclusion)
+    return roundResult;
 }
 
+/* This function initiates a game and loops through the number of rounds.
+The function prompts user for input until it is valid utilizing while loop.
+The round results are calculated by previously created function. 
+Return values from the function are used to calculate sum of rounds.
+If the sum of rounds is negative, it means that the player, lost.
+For 0 its a draw.
+For positive it is a win.*/
+
+function game() {
+    let keepGoing;
+    let playerChoice;
+    let gameResult = 0;
+
+    for (let i = 0; i < 5; i++) {
+        keepGoing = true;
+        while (keepGoing) {
+            playerChoice = prompt("Choose rock, paper or scissors.").toLowerCase();
+            if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors") {
+                keepGoing = false;
+            }
+        }
+        gameResult += calcRoundResults(playerChoice, getComputerChoice());
+    }
+
+    if (gameResult > 0) {
+        console.log("You won!")
+    } else if (gameResult < 0) {
+        console.log("You lose!")
+    } else {
+        console.log("Its a draw!")
+    }
+}
+
+// call the game
+game()
